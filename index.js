3a7c7889c65fd5d2a827475942572c0056d0c6fd
cgorrino <cgorrino03@gmail.com>
2021-05-03 23:43:38 -0700

    Adding in setup for node project and beginning the layout of the README template with the input from inquirer.

---
+++ index.js
@@ -0,0 +1,97 @@
+const inquirer = require('inquirer')
+const fs = require('fs');
+const { title } = require('node:process');
+
+
+// Get user input
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your project title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Enter a short description of your project',
+      name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'How do you install it?',
+        name: 'installation',
+      },
+    {
+      type: 'input',
+      message: 'How do you use it?',
+      name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'How should someone contribute?',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'How do you run tests?',
+        name: 'testing',
+    },
+    // Add list type to pick license
+    //
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    }
+  ])
+  .then(({title, description, installation, usage, contributing, testing, github, email}) =>{
+
+    const content = `
+# ${title}
+
+## Description
+
+${description}
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## How to contribute
+
+${contributing}
+
+## How to run tests
+
+${testing}
+
+## License
+
+## Questions
+
+Contact me via email at ${email} or through my GitHub [profile](https://github.com/${github}). 
+
+`
+fs.writeFile("README.md", content, (err) =>
+err ? console.error(err) : console.log("Success!"));
+
+  }
+
+  );
+
+
+
+
+
+
+
+  
